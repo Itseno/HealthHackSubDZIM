60940b01a24a9f83f1fe0d18f48004ebcc681d13
Ian Crossey <crossei@stlprioryschool.org>
2018-04-07 15:41:37 -0500

    JQuery Stuff

--- UserPage/UserData.js
+++ UserPage/UserData.js
@@ -1,14 +1,27 @@
-function UserI() {
-var x = $("#UserInfo").serializeArray();
-console.log(x);
+var UserI = function(){
+  var x = $("#UserInfo").serializeArray();
+  console.log(x);
 
-var PharmacyName = x[0].value;
-var PharmicistName = x[1];
-var PharmicyPhone = x[2];
-var PECName = x[3];
-var PECPhone = x[4];
-var SECName = x[5];
-var SECPhone = x[6];
-
-console.log(SECPhone);
+  // console.log(PharmacyName);
+  localStorage.setItem('formData', JSON.stringify(x));
 }
+
+window.onload = UserI();
+
+var saveToLocalStorage = function (){
+  PharmacyName = localStorage.getItem(x[0]);
+  PharmicistName = localStorage.getItem(x[1]);
+  PharmicyPhone = localStorage.getItem(x[2]);
+  PECName = localStorage.getItem(x[3]);
+  PECPhone = localStorage.getItem(x[4]);
+  SECName = localStorage.getItem(x[5]);
+  SECPhone = localStorage.getItem(x[6]);
+}
+
+// var PharmacyName = localStorage.getItem(x[0]);
+// var PharmicistName = localStorage.getItem(x[1]);
+// var PharmicyPhone = localStorage.getItem(x[2]);
+// var PECName = localStorage.getItem(x[3]);
+// var PECPhone = localStorage.getItem(x[4]);
+// var SECName = localStorage.getItem(x[5]);
+// var SECPhone = localStorage.getItem(x[6]);
